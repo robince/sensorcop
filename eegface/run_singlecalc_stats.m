ccf2e0b88e55a1ffb91751a4fc17368e8880280e
Robin Ince <robince@gmail.com>
2014-08-20 10:43:21 +0100

    trial dependence for eeg face data

---
+++ eegface/run_singlecalc_stats.m
@@ -0,0 +1,218 @@
+% run trial-dependendent permutation tests of various statistical tests
+
+subid = 'HKI1';
+fname = sprintf('%s_ridat.mat',subid);
+dat = load(fullfile(data_dir, fname));
+
+time = dat.time;
+stim = dat.stim;
+% rspdat = permute(dat.eegdat, [3 2 1]);
+rspdat = permute(dat.csddat, [3 2 1]);
+[Ntrl Nt Nchan] = size(rspdat);
+
+%%
+% max effect
+ci = 41; % B9
+% ti = 319;
+ti = 294;
+
+rsp = rspdat(:,ti,ci);
+rsp0 = rsp(stim==0);
+rsp1 = rsp(stim==1);
+trlnums = [8 16 32 64 128 256 512 1024];
+Ntrlnum = length(trlnums);
+Nperm = 100;
+
+% select trials
+Ntrlrep = 100;
+trldat = cell(Ntrlrep,Ntrlnum);
+trlstm = cell(1,Ntrlnum);
+trlperm = cell(1,Ntrlnum);
+for i=1:Ntrlnum
+    Nts = trlnums(i) / 2;
+    for ri=1:Ntrlrep
+%         trldat{ri,i} = [ rsp0(randperm(length(rsp0),Nts)); rsp1(randperm(length(rsp1),Nts)) ];        
+        trldat{ri,i} = [ rsp0(randi(length(rsp0),[Nts 1])); rsp1(randi(length(rsp1),[Nts 1])) ];
+    end
+    trlstm{i} = [ zeros(Nts,1); ones(Nts,1) ];
+    
+    permdat = zeros(trlnums(i),Nperm);
+    for pi=1:Nperm
+        % without replacement
+        permdat(:,pi) = randperm(trlnums(i));
+        % with replacement
+%          permdat(:,pi) = randi(trlnums(i),[trlnums(i) 1]);
+    end
+    trlperm{i} = permdat;
+end
+
+fname = sprintf('%s_csd_%d_%d_trialselect.mat',subid,ci,ti);
+save(fullfile(data_dir,fname), 'trldat','trlstm','trlperm')
+
+%%
+ci = 41; % B9
+% ti = 319;
+ti = 294;
+fname = sprintf('%s_csd_%d_%d_trialselect.mat',subid,ci,ti);
+load(fullfile(data_dir,fname), 'trldat','trlstm','trlperm')
+
+Ntrlnum = length(trlstm);
+Nperm = size(trlperm{1},2);
+Ntrlrep = size(trldat,1);
+
+tic
+trlres = cell(1,Ntrlnum);
+parfor i=1:Ntrlnum
+    % quantise
+    res = [];
+    res.Ic = zeros(1,Ntrlrep);
+    res.Icperm = zeros(Nperm,Ntrlrep);
+    res.Ig = zeros(1,Ntrlrep);
+    res.Igperm = zeros(Nperm,Ntrlrep);
+    res.Ib2 = zeros(1,Ntrlrep);
+    res.Ib2perm = zeros(Nperm,Ntrlrep);
+    res.Ib4 = zeros(1,Ntrlrep);
+    res.Ib4perm = zeros(Nperm,Ntrlrep);
+    res.Ib8 = zeros(1,Ntrlrep);
+    res.Ib8perm = zeros(Nperm,Ntrlrep);
+    res.t = zeros(1,Ntrlrep);
+    res.tperm = zeros(Nperm,Ntrlrep);
+    res.ks = zeros(1,Ntrlrep);
+    res.ksperm = zeros(Nperm,Ntrlrep);   
+    
+    Ntrl = size(trldat{1,i},1);
+    for ri=1:Ntrlrep
+        rdat = trldat{ri,i};
+        cdat = copnorm(rdat);
+        qdat2 = int16(bin.eqpop(rdat,2));
+        qdat4 = int16(bin.eqpop(rdat,4));
+        qdat8 = int16(bin.eqpop(rdat,8));
+        
+        % 2 sample format
+        rdat0 = rdat(1:(Ntrl/2));
+        rdat1 = rdat(((Ntrl/2)+1):end);
+        
+        % true val
+        res.Ic(ri) = info_gd(cdat, trlstm{i}, 2, true, true, false);
+        res.Ig(ri) = info_gd(rdat, trlstm{i}, 2, true, false, false);
+        qstm = int16(trlstm{i});
+        res.Ib2(ri) = info.calc_info_integer_c_int16_t(qdat2,2,qstm,2,Ntrl);
+        res.Ib4(ri) = info.calc_info_integer_c_int16_t(qdat4,4,qstm,2,Ntrl);
+        res.Ib8(ri) = info.calc_info_integer_c_int16_t(qdat8,8,qstm,2,Ntrl);
+        
+        [h, p, ci, stat] = ttest2(rdat0,rdat1);
+        res.t(ri) = stat.tstat;
+        [h, p, ksstat] = kstest2(rdat0,rdat1);
+        res.ks(ri) = ksstat;
+
+        % permutations
+        for pi=1:Nperm
+            stm = trlstm{i}(trlperm{i}(:,pi));
+            rdat0 = rdat(stm==0);
+            rdat1 = rdat(stm==1);
+            qstm = int16(stm);
+            res.Icperm(pi,ri) = info_gd(cdat, stm, 2, true, true, false);
+            res.Igperm(pi,ri) = info_gd(rdat, stm, 2, true, false, false);
+            res.Ib2perm(pi,ri) = info.calc_info_integer_c_int16_t(qdat2,2,qstm,2,Ntrl);
+            res.Ib4perm(pi,ri) = info.calc_info_integer_c_int16_t(qdat4,4,qstm,2,Ntrl);
+            res.Ib8perm(pi,ri) = info.calc_info_integer_c_int16_t(qdat8,8,qstm,2,Ntrl);
+            [h, p, ci, stat] = ttest2(rdat0,rdat1);
+            res.tperm(pi,ri) = stat.tstat;
+            [h, p, ksstat] = kstest2(rdat0,rdat1);
+            res.ksperm(pi,ri) = ksstat;
+        end
+    end
+    trlres{i} = res;
+end
+toc
+
+%% z-score stats
+stats = {'Ic' 'Ig' 'Ib2' 'Ib4' 'Ib8' 't' 'ks'};
+
+for i=1:Ntrlnum
+    res = trlres{i};
+    for sti=1:length(stats)
+        stat = stats{sti};
+        
+        res.([stat 'Z']) = zeros(1,Ntrlrep);
+        for ri=1:Ntrlrep
+            mu = mean(res.([stat 'perm'])(:,ri));
+            sigma = std(res.([stat 'perm'])(:,ri));
+            res.([stat 'Z'])(ri) = (res.(stat)(ri) - mu) / sigma;
+        end
+    end
+    trlres{i} = res;
+end
+
+%% dat for graph
+stats = {'Ic' 'Ig' 'Ib2' 'Ib4' 'Ib8' 't' 'ks'};
+pltavg = [];
+pltstd = [];
+
+for sti=1:length(stats)
+    stat = stats{sti};
+    
+    pltavg.(stat) = zeros(1,Ntrlnum);
+    pltstd.(stat) = zeros(1,Ntrlnum);
+    
+    for i=1:Ntrlnum   
+        pltavg.(stat)(i) = mean(abs(trlres{i}.([stat 'Z'])));
+        pltstd.(stat)(i) = std(abs(trlres{i}.([stat 'Z'])));
+    end
+end
+
+figure
+for sti=1:length(stats)
+    stat = stats{sti};
+    loglog(trlnums, pltavg.(stat))
+    hold all
+end
+legend(stats)
+
+%%
+stats = {'Ic' 'Ig' 'Ib2' 'Ib4' 'Ib8'};
+pltavg = [];
+pltstd = [];
+
+for sti=1:length(stats)
+    stat = stats{sti};
+    
+    pltavg.(stat) = zeros(1,Ntrlnum);
+    pltstd.(stat) = zeros(1,Ntrlnum);
+    
+    for i=1:Ntrlnum   
+        pltavg.(stat)(i) = mean(abs(trlres{i}.([stat])));
+        pltstd.(stat)(i) = std(abs(trlres{i}.([stat])));
+    end
+end
+figure
+for sti=1:length(stats)
+    stat = stats{sti};
+    semilogx(trlnums, pltavg.(stat))
+    hold all
+end
+legend(stats)
+%%
+stats = {'t' 'ks'};
+pltavg = [];
+pltstd = [];
+
+for sti=1:length(stats)
+    stat = stats{sti};
+    
+    pltavg.(stat) = zeros(1,Ntrlnum);
+    pltstd.(stat) = zeros(1,Ntrlnum);
+    
+    for i=1:Ntrlnum   
+        pltavg.(stat)(i) = mean(abs(trlres{i}.([stat])));
+        pltstd.(stat)(i) = std(abs(trlres{i}.([stat])));
+    end
+end
+figure
+for sti=1:length(stats)
+    stat = stats{sti};
+    semilogx(trlnums, pltavg.(stat))
+    hold all
+end
+legend(stats)
+ 
---
+++ eegface/sensor_info.m
@@ -0,0 +1,72 @@
+% calculate sensor info to pick a good channel
+
+subid = 'HKI1';
+fname = sprintf('%s_ridat.mat',subid);
+dat = load(fullfile(data_dir, fname));
+
+%%
+time = dat.time;
+stim = dat.stim;
+% rspdat = permute(dat.eegdat, [3 2 1]);
+rspdat = permute(dat.csddat, [3 2 1]);
+[Ntrl Nt Nchan] = size(rspdat);
+
+qdat = copnorm(rspdat);
+%%
+I = zeros(Nt, Nchan);
+for ci=1:Nchan
+    for ti=1:Nt
+        I(ti,ci) = info_gd(qdat(:,ti,ci), stim, 2, true, true, false);
+    end
+end
+
+%%
+minlim = min(I(:));
+maxlim = max(I(:));
+
+tidx = find((time>0) & (time<350));
+
+%%
+clear F
+for tii=1:length(tidx)
+    ti = tidx(tii);
+    t = I(ti,:);
+    cla
+    topoplot(t, dat.chanlocs, 'maplimits', [minlim maxlim]);
+    title(sprintf('%d ms', time(ti)),'FontSize',12);
+    F(tii) = getframe(gcf);
+end
+
+% topoplot(mean(DATA(:,170,:),3),EEG.locs)
+
+fname = sprintf('csd_face_info.mov');
+mo = QTWriter(fname);
+mo.FrameRate = 5;
+mo.Loop = 'loop';
+
+for i=1:length(F)
+    writeMovie(mo, F(i));
+end
+close(mo);
+close all
+
+%%
+ci = 41; % chan
+tidx = find((time>-100) & (time<350));
+figure
+subplot(311)
+hold on
+erp0 = mean(rspdat(stim==0,tidx,ci));
+erp1 = mean(rspdat(stim==1,tidx,ci));
+plot(time(tidx), erp0,'r')
+plot(time(tidx), erp1,'b')
+legend('Face','Noise')
+title('ERPs')
+
+subplot(312)
+plot(time(tidx),erp1-erp0,'k');
+title('ERP DIFF')
+
+subplot(313)
+plot(time(tidx),I(tidx,ci),'k');
+title('Info')
