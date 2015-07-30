9a26c4645d68dc798ff49f278f301fd4c030abd6
Robin Ince <robince@gmail.com>
2015-07-30 16:07:51 +0100

    fast rocarea

---
+++ make.m
@@ -0,0 +1,88 @@
+%
+% make.m - build script for MatlabAPI_lite
+%
+
+% set to false if you don't have PyF95++ and are just using checked out
+% sources
+PROCESS_TEMPLATES = false;
+% mex options
+DEBUG = false;
+VERBOSE = false;
+LARGEARRAY = true;
+
+% path to matlab api
+MATLABAPI_DIR = '/Users/robince/git/MatlabAPI_lite';
+
+% Matlab doesn't use the normal system path so need the full path to
+% executables
+PYF95 = '/Users/robince/slash/bin/PyF95++';
+% PyF95++ installs to a different python directory so need to set
+% PYTHONPATH appropriately
+PYF95_PP = '/Users/robince/slash/lib/python2.7/site-packages';
+setenv('PYTHONPATH', PYF95_PP);
+% I had to change the !# line in the PyF95++ to explicitly point to the
+% current python since env inside Matlab gives the wrong one because of the
+% path
+
+if ispc
+    OBJEXT = 'obj';
+else
+    OBJEXT = 'o';
+end
+
+%% Clean
+
+if PROCESS_TEMPLATES
+    delete *.F90
+    delete *.f90
+end
+delete *.mod
+delete *.i90
+delete(['*.' OBJEXT])
+delete(['*.' mexext])
+% cd tests
+% if PROCESS_TEMPLATES
+%     delete *.F90
+%     delete *.f90
+% end
+% delete *.mod
+% delete(['*.' OBJEXT])
+% delete(['*.' mexext])
+% cd ..
+%% Process templates
+
+if PROCESS_TEMPLATES
+    st = system([ PYF95 ' --sources="fcinfo.F90T instantiate.F90T copnorm.F90T copnorm_slice_omp.F90T" --std="f03" -x'],'-echo');
+    if st ~= 0
+        error('Problem processing MatlabAPImx.F90T template')
+    end
+end
+%% Build mex
+
+ARGS = {};
+if LARGEARRAY,  ARGS{end+1} = '-largeArrayDims';
+else            ARGS{end+1} = '-compatibleArrayDims'; end
+if DEBUG,       ARGS{end+1} = '-g'; end
+if VERBOSE,     ARGS{end+1} = '-v'; end
+ARGS{end+1} = ['-I' MATLABAPI_DIR];
+% ARGS{end+1} = ['-I/opt/intel/composer_xe_2011_sp1.9.289/mkl/include/'];
+
+typekind = {'c_double' 'c_float'};
+%%
+% lib_array
+MEXARGS = ARGS;
+MEXARGS{end+1} = '-c';
+MEXARGS{end+1} = 'lib_array.f';
+mex(MEXARGS{:})
+
+
+
+%%
+MEXARGS = ARGS;
+MEXARGS{end+1} = 'rocarea.f';
+MEXARGS{end+1} = fullfile(MATLABAPI_DIR,['MatlabAPImx.' OBJEXT]);
+MEXARGS{end+1} = fullfile(MATLABAPI_DIR,['MatlabAPImex.' OBJEXT]);
+MEXARGS{end+1} = ['lib_array.' OBJEXT];
+MEXARGS{end+1} = '-lmwblas';
+MEXARGS{end+1} = '-lmwlapack';
+mex(MEXARGS{:})
---
+++ rocarea.f
@@ -0,0 +1,89 @@
+#include "fintrf.h"
+
+subroutine mexFunction(nlhs, plhs, nrhs, prhs)
+    use iso_c_binding
+    use MatlabAPImx
+    use MatlabAPImex
+    implicit none
+! ARG
+    mwPointer :: plhs(*), prhs(*)
+    integer :: nlhs, nrhs
+! LOC
+    real(c_double), pointer :: p(:), Az
+    integer(c_int16_t), pointer :: label(:)
+    mwPointer :: mxAz
+    mwSize, parameter :: One=1
+    mwSize :: Ntrl
+
+    if( nrhs /= 2 ) then
+        call mexErrMsgTxt("This function takes 2 inputs")
+    endif
+    if( nlhs > 1) then
+        call mexErrMsgTxt("This function returns 1 output")
+    endif
+
+    call fpGetPr( p,  prhs(1) )
+    call fpGetPr( label,  prhs(2) )
+
+    if( (.not.associated(p)) .or. (.not.associated(label)) ) then
+        call mexErrMsgTxt("Problem with inputs: check types and dimensions")
+    endif
+
+    Ntrl = size(p)
+    if( (size(label)/=Ntrl) ) then
+        call mexErrMsgTxt("Number of trials does not match")
+    endif
+
+    ! allocate Matlab memory
+    mxAz = mxCreateNumericMatrix(One, One, mxDOUBLE_CLASS, mxREAL)
+    call fpGetPr( Az, mxAz )
+    plhs(1) = mxAz
+
+    call dorocarea(p, label, Ntrl, Az)
+
+end subroutine mexFunction
+
+subroutine dorocarea(p, label, Ntrl, Az)
+    use iso_c_binding
+    use lib_array
+    implicit none
+    ! ARG
+    mwSize, intent(in) :: Ntrl
+    real(c_double), intent(in) :: p(Ntrl)
+    integer(c_int16_t), intent(in) :: label(Ntrl)
+    real(c_double), intent(out) :: Az
+    ! LOC
+    integer :: index(Ntrl), i, Np, Nn
+    real(c_double) :: cp(Ntrl), tp, denNp, denNn
+
+    Nn = 0
+    Np = 0
+    do i=1,Ntrl
+      if( label(i) == 0 ) then 
+        Nn = Nn+1
+      else 
+        Np = Np+1
+      endif
+    end do
+
+    Az = 0.0
+
+    ! argsort data
+    index = (/ (i, i=1,Ntrl) /)
+    cp = p
+    call quicksort_index(cp,index)
+
+    Az = 0.0
+    tp = 0.0
+    denNp = 1.0_8 / real(Np,8)
+    denNn = 1.0_8 / real(Nn,8)
+    do i=1,Ntrl
+      if( label(index(i))==1 ) then
+        tp = tp + label(index(i))*denNp
+      else
+        ! label(index(i)) == 0
+        Az = Az + tp*denNn
+      endif
+    end do
+end subroutine dorocarea
+
