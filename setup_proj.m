b5820854e5da3ed570411fe7e5836a53c951699d
Robin Ince <robince@gmail.com>
2015-08-19 15:55:32 +0100

    setup_proj for mac also

--- setup_proj.m
+++ setup_proj.m
@@ -1,12 +1,21 @@
 name = getComputerName();
 if strcmp(name(1:10), 'robini2-pc')
     % robins workstation
-    h.data_dir = '/data/hd/sensorcop';
+    data_dir = '/data/hd/sensorcop';
+    addpath('/home/robini/code/gauss_info/')
+    addpath('/home/robini/code/gauss_info/mex/')
+    addpath('/home/robini/code/info/')
+elseif strcmp(mexext,'mexmaci64')
+    data_dir = '~/Documents/gladata/sensorcop';
+    addpath('~/Documents/glacode/para_info/')
+    addpath('~/Documents/glacode/para_info/mex/')
+    addpath('~/Documents/glacode/info/')
 else
     % on the grid
-    h.data_dir = '/analyse/Project0110/robin/sensorcop';
+    data_dir = '/analyse/Project0110/robin/sensorcop';
+    addpath('/home/robini/code/gauss_info/')
+    addpath('/home/robini/code/gauss_info/mex/')
+    addpath('/home/robini/code/info/')
 end
 
-addpath('/home/robini/code/gauss_info/')
-addpath('/home/robini/code/gauss_info/mex/')
-addpath('/home/robini/code/info/')
+
