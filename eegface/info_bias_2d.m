49bae895d9038974f10eac285b727fdd20d7fd42
Robin Ince <robince@gmail.com>
2016-07-07 15:09:26 +0100

    update bias plots for eegface (include kraskov)

--- eegface/info_bias_2d.m
+++ eegface/info_bias_2d.m
@@ -29,87 +29,123 @@ ti = 67;
 % ti = 45;
 
 
-log_samp_size = 3:10;
+log_samp_size = 4:10;
 Nsamp = length(log_samp_size);
 
-Nrep = 600;
-res.Ib2 = zeros(Nrep, Nsamp);
-res.Ib4 = zeros(Nrep, Nsamp);
-res.Ib8 = zeros(Nrep, Nsamp);
-res.Ib2mm = zeros(Nrep, Nsamp);
-res.Ib4mm = zeros(Nrep, Nsamp);
-res.Ib8mm = zeros(Nrep, Nsamp);
-res.Icop = zeros(Nrep, Nsamp);
+Nrep = 500;
+Ib2 = zeros(Nrep, Nsamp);
+Ib4 = zeros(Nrep, Nsamp);
+Ib2mm = zeros(Nrep, Nsamp);
+Ib4mm = zeros(Nrep, Nsamp);
+Icop = zeros(Nrep, Nsamp);
+Ik = zeros(Nrep,Nsamp);
+
 Nthread = 4;
 for sampi=1:Nsamp
     thsNtrl = 2.^(log_samp_size(sampi))
-    for repi=1:Nrep
-        idx = randperm(Ntrl, thsNtrl);
+    parfor repi=1:Nrep
+%         idx = randperm(Ntrl, thsNtrl);
+        % bootstrap (with replacement)
+        idx = randi(Ntrl,thsNtrl,1);
         thsstim = stim(idx);
         ss = sum(thsstim==0);
-        while (ss<2 || (thsNtrl-ss)<2)
-            idx = randperm(Ntrl, thsNtrl);
+        while (ss<4 || (thsNtrl-ss)<4)
+%             idx = randperm(Ntrl, thsNtrl);
+            idx = randi(Ntrl,thsNtrl,1);    
             thsstim = stim(idx);
             ss = sum(thsstim==0);
         end
         thseeg = rspdat(idx,:,ti,chi);
         qstm = int16(thsstim);
-        
+        % add jitter to avoid equal elements
+        thseeg = thseeg + 1e-6*randn(size(thseeg));
         % true values
         rqrsp2 = double(bin.eqpop(squeeze(thseeg(:,1)),2));
         dqrsp2 = double(bin.eqpop(squeeze(thseeg(:,2)),2));
         qrsp2 = numbase2dec([rqrsp2(:) dqrsp2(:)]',2);
         qrsp2 = int16(reshape(qrsp2, [thsNtrl 1]));
-        res.Ib2(repi,sampi) = info.calc_info_integer_c_int16_t(qrsp2, 4, qstm, 2, thsNtrl);
-        res.Ib2mm(repi,sampi) = res.Ib2(repi,sampi) - 3./(2*thsNtrl*log(2));
+        Ib2(repi,sampi) = info.calc_info_integer_c_int16_t(qrsp2, 4, qstm, 2, thsNtrl);
+        Ib2mm(repi,sampi) = Ib2(repi,sampi) - 3./(2*thsNtrl*log(2));
 
         rqrsp4 = double(bin.eqpop(squeeze(thseeg(:,1)),4));
         dqrsp4 = double(bin.eqpop(squeeze(thseeg(:,2)),4));
         qrsp4 = numbase2dec([rqrsp4(:) dqrsp4(:)]',4);
         qrsp4 = int16(reshape(qrsp4, [thsNtrl 1]));
-        res.Ib4(repi,sampi) = info.calc_info_integer_c_int16_t(qrsp4, 16, qstm, 2, thsNtrl);
-        res.Ib4mm(repi,sampi) = res.Ib4(repi,sampi) - 15./(2*thsNtrl*log(2));
+        Ib4(repi,sampi) = info.calc_info_integer_c_int16_t(qrsp4, 16, qstm, 2, thsNtrl);
+        Ib4mm(repi,sampi) = Ib4(repi,sampi) - 15./(2*thsNtrl*log(2));
 
-        rqrsp8 = double(bin.eqpop(squeeze(thseeg(:,1)),8));
-        dqrsp8 = double(bin.eqpop(squeeze(thseeg(:,2)),8));
-        qrsp8 = numbase2dec([rqrsp8(:) dqrsp8(:)]',8);
-        qrsp8 = int16(reshape(qrsp8, [thsNtrl 1]));
-        res.Ib8(repi,sampi) = info.calc_info_integer_c_int16_t(qrsp8, 64, qstm, 2, thsNtrl);
-        res.Ib8mm(repi,sampi) = res.Ib8(repi,sampi) - 63./(2*thsNtrl*log(2));
-        
         crsp = copnorm(thseeg);
-%         try
-            res.Icop(repi,sampi) = info_gd(crsp, qstm, 2, true, false, false);
-%         catch
-%             res.Icop(repi,sampi) = NaN;
-%         end
-        if ~isfinite(res.Icop(repi,sampi))
-            keyboard
-        end
+        Icop(repi,sampi) = info_gd(crsp, qstm, 2, true, false, false);
+        
+        % Kraskov
+        k = 3;
+        Hunc = KraskovEntropyV2(thseeg,k);
+        w = zeros(1,2);
+        Hcond = zeros(1,2);
+        % class 0
+        cidx = qstm==0;
+        w(1) = sum(cidx) ./ thsNtrl;
+        Hcond(1) = KraskovEntropyV2(thseeg(cidx,:),k);
+        % class 1
+        cidx = qstm==1;
+        w(2) = sum(cidx) ./ thsNtrl;
+        Hcond(2) = KraskovEntropyV2(thseeg(cidx,:),k);
+        Ik(repi,sampi) = (Hunc - sum(w .* Hcond))/log(2);
     end
 end
+%%
+clear dat rspdat drsp stim
+save('2dbias_boot500.mat')
+%%
+est = { 'Ib2mm' 'Ib4mm'  'Icop' 'Ik'};
+res = load('2dbias_boot500.mat');
+figure
+hold all
+for ei=1:length(est)
+    plot(log_samp_size, nanmean(res.(est{ei}),1));
+end
+legend(est)
+%%
+% est = {'Ib2' 'Ib4' 'Ib8' 'Icop'};
+% figure
+% hold all
+% for ei=1:length(est)
+%     plot(log_samp_size, nanstd(res.(est{ei}),1));
+% end
+% legend(est)
+% %%
+% est = {'Ib4'  'Icop'};
+% figure
+% hold all
+% for ei=1:length(est)
+%     errorbar(2.^log_samp_size, nanmean(res.(est{ei}),1),nanstd(res.(est{ei}),[],1));
+% end
+% legend(est)
 
 %%
-est = {'Ib2' 'Ib4' 'Ib8' 'Ib2mm' 'Ib4mm' 'Ib8mm' 'Icop'};
+sidx = 1:Nsamp;
+x = log_samp_size(sidx);
+
 figure
+
+subplot(1,1,1)
+pd = squeeze(Icop(:,sidx));
+errorbar(x-0.1, mean(pd), mean(pd)-prctile(pd,25),prctile(pd,75)-mean(pd));
 hold all
-for ei=1:length(est)
-    plot(2.^log_samp_size, nanmean(res.(est{ei}),1));
-end
-legend(est)
-%%
-est = {'Ib2' 'Ib4' 'Ib8' 'Icop'};
-figure
-hold all
-for ei=1:length(est)
-    plot(log_samp_size, nanstd(res.(est{ei}),1));
-end
-legend(est)
-%%
-est = {'Ib4'  'Icop'};
-figure
-hold all
-for ei=1:length(est)
-    errorbar(2.^log_samp_size, nanmean(res.(est{ei}),1),nanstd(res.(est{ei}),[],1));
-end
-legend(est)
+
+pd = squeeze(Ib2mm(:,sidx));
+errorbar(x-0.05, median(pd), median(pd)-prctile(pd,25),prctile(pd,75)-median(pd));
+
+pd = squeeze(Ib4mm(:,sidx));
+errorbar(x+0.05, mean(pd), mean(pd)-prctile(pd,25),prctile(pd,75)-mean(pd));
+
+pd = squeeze(Ik(:,sidx));
+errorbar(x+0.1, mean(pd), mean(pd)-prctile(pd,25),prctile(pd,75)-mean(pd));
+
+xlim([3.5 10.5])
+% ylim([-0.15 1])
+
+legend('GCMI','2 bin','4 bin','kNN')
+ylabel('MI (bits)')
+xlabel('log_2 samples')t
+% subplot(2,1,2)
