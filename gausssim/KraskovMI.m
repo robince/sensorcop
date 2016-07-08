404981715d8b83ecc0f3480384de5162803255e2
Robin Ince <robince@gmail.com>
2016-07-08 16:38:58 +0100

    performance optimizations for Krakov MI

--- gausssim/KraskovMI.m
+++ gausssim/KraskovMI.m
@@ -11,7 +11,7 @@ function [ I1, I2 ] = KraskovMI( X, Y, k, varargin )
 %
 %   2. Output: I1, I2: the two estimator of MI, I(1), I(2) (see Ref.)
 %
-% Ref: Kraskov, Alexander, Harald Stögbauer, and Peter Grassberger.
+% Ref: Kraskov, Alexander, Harald Stï¿½gbauer, and Peter Grassberger.
 %      "Estimating mutual information." Physical review E 69.6 (2004): 066138.
 %
 % Author: Paolo Inglese <paolo.ingls@gmail.com>
@@ -39,16 +39,20 @@ end
 nObs = size(X, 1);
 
 % compute distance between each sample and its k-th nearest neighbour
-dz = zeros(nObs, nObs);
-dx = zeros(nObs, nObs);
-dy = zeros(nObs, nObs);
-for i = 1:nObs
-    for j = 1:nObs
-        dx(i,j) = sqrt(sum((X(i, :) - X(j, :)).^2));
-        dy(i,j) = sqrt(sum((Y(i, :) - Y(j, :)).^2));
-        dz(i,j) = max([dx(i, j), dy(i, j)]);
-    end
-end
+% dz = zeros(nObs, nObs);
+% dx = zeros(nObs, nObs);
+% dy = zeros(nObs, nObs);
+% for i = 1:nObs
+%     for j = 1:nObs
+%         dx(i,j) = sqrt(sum((X(i, :) - X(j, :)).^2));
+%         dy(i,j) = sqrt(sum((Y(i, :) - Y(j, :)).^2));
+%         dz(i,j) = max([dx(i, j), dy(i, j)]);
+%     end
+% end
+
+dx = squareform(pdist(X));
+dy = squareform(pdist(Y));
+dz = max(dx,dy);
 
 % find nx(i) and ny(i)
 Eps = zeros(nObs, 1);
@@ -56,44 +60,60 @@ Nn = zeros(nObs, 1);
 
 nx1 = zeros(nObs, 1);
 ny1 = zeros(nObs, 1);
-nx2 = zeros(nObs, 1);
-ny2 = zeros(nObs, 1);
+% nx2 = zeros(nObs, 1);
+% ny2 = zeros(nObs, 1);
+% xns = createns(X);
+% yns = createns(Y);
+
 for i = 1:nObs
     
-    dxSample = dx(i, :);
-    dxSample(i) = [];
+    dxSample = dx(:,i);
+%     dxSample(i) = [];
     
-    dySample = dy(i, :);
-    dySample(i) = [];
+    dySample = dy(:,i);
+%     dySample(i) = [];
     
-    dzSample = dz(i, :);
-    dzSample(i) = [];
-    [EpsSample, NnSample] = sort(dzSample, 'ascend');
+    dzSample = dz(:,i);
+%     dzSample = max(dxSample,dySample);
+    dzSample(i) = Inf;
+%     [EpsSample, NnSample] = sort(dzSample, 'ascend');
+%     EpsSample = sort(dzSample, 'ascend');
+%     EpsSample = mink(dzSample,k);
+    EpsSample = minkmex(dzSample,k);
     Eps(i) = EpsSample(k);
-    Nn(i) = NnSample(k);
+%     Nn(i) = NnSample(k);
     
-    nx1(i) = sum(dxSample < Eps(i));
-    ny1(i) = sum(dySample < Eps(i));
+    nx1(i) = sum(dxSample < Eps(i))-1;
+    ny1(i) = sum(dySample < Eps(i))-1;
     
-    nx2(i) = sum(dxSample <= Eps(i));
-    ny2(i) = sum(dySample <= Eps(i));
-    
+%     idx = rangesearch(xns,X(i,:),Eps(i)-eps(Eps(i)));
+%     nx1(i) = length(idx{1})-1;
+%     idx = rangesearch(yns,Y(i,:),Eps(i)-eps(Eps(i)));
+%     ny1(i) = length(idx{1})-1;
+
+
+%     nx2(i) = sum(dxSample <= Eps(i));
+%     ny2(i) = sum(dySample <= Eps(i));
+%     
 end
+
+% [~, distEpsilon] = knnsearch(X, X, 'K', k+1, 'distance', distance);
+% distEpsilon(:, 1) = []; % remove self-distance (all zeros)
 
 % mutual information estimators
 I1 = psi(k) - sum(psi(nx1 + 1) + psi(ny1 + 1)) / nObs + psi(nObs);
-I2 = psi(k) - 1/k - sum(psi(nx2) + psi(ny2)) / nObs + psi(nObs);
-
-if (zeroFix)
-    if I1 < 0
-        warning('First estimator is negative -> 0');
-        I1 = 0;
-    end
-    if I2 < 0
-        warning('Second estimator is negative -> 0');
-        I2 = 0;
-    end
-end
+% I2 = psi(k) - 1/k - sum(psi(nx2) + psi(ny2)) / nObs + psi(nObs);
+% 
+% if (zeroFix)
+%     if I1 < 0
+%         warning('First estimator is negative -> 0');
+%         I1 = 0;
+%     end
+%     if I2 < 0
+%         warning('Second estimator is negative -> 0');
+%         I2 = 0;
+%     end
+% end
 
 end
 
